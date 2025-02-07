1dba7587a34637c1adb2b380f2137d3ba1d26269
Rzeigui Ahmed <ahmedrzeigui52@gmail.com>
2025-02-07 14:03:03 +0100

    Création des classes : Categorie Livre et Utilisateur

---
+++ GestionBibliothequeRzeiguiAhmed/Utilisateur.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+
+public class Utilisateur
+{
+	public string Nom {  get; set; }
+	public string Prenom { get; set; }
+	public string Email { get; set; }
+	public DateFormat DateInscription { get; set; }
+
+	public	List<Livre> LivresEmpruntes { get; set; }
+
+	public Utilisateur()
+	{
+
+	}
+}
