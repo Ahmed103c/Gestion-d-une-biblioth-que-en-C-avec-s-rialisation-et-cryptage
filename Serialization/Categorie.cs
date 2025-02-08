dd25a0bb7509d9529ddd534b9a9dee2d6267bdee
Rzeigui Ahmed <ahmedrzeigui52@gmail.com>
2025-02-08 19:37:42 +0100

    Création projet Cryptage

---
+++ Serialization/Categorie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+public class Categorie
+{
+	public string Nom {  get; set; }
+	List<Livre> Livres { get; set; }
+	public Categorie()
+	{
+
+	}
+}
--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -6,16 +6,16 @@ using System.Text.Json;
 using System.IO.Compression;
 using System.Xml.Serialization;
 
-string chemin = "C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\Serialization_Cryptage\\Bibliotheque";
+string chemin = "C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\Serialization\\Bibliotheque";
 Utilisateur u3 = new Utilisateur("Amine");
 SerializationFactory.sauvegarder(chemin, TypeSerialization.binaire, u3,"1234");
 Utilisateur u4 = new Utilisateur("Arij");
 SerializationFactory.sauvegarder(chemin, TypeSerialization.binaire, u4, "1235");
 
-string chemin1 = "C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\Serialization_Cryptage\\Bibliotheque\\Utilisateur_Arij.bjson";
+string chemin1 = "C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\Serialization\\Bibliotheque\\Utilisateur_Arij.bjson";
 Utilisateur utilisateur = (Utilisateur)SerializationFactory.Charger(chemin1, typeof(Utilisateur), TypeSerialization.binaire);
 Console.WriteLine($"Nom : {utilisateur.Nom}");
 
-string chemin2 = "C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\Serialization_Cryptage\\Bibliotheque\\Utilisateur_Amine.bjson";
+string chemin2 = "C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\Serialization\\Bibliotheque\\Utilisateur_Amine.bjson";
 Utilisateur utilisateur2 = (Utilisateur)SerializationFactory.Charger(chemin2, typeof(Utilisateur), TypeSerialization.binaire);
 Console.WriteLine($"Nom : {utilisateur2.Nom}");
