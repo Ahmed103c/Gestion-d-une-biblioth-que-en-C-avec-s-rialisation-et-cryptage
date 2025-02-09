fb3ec869ed61de864a1fa76ea8432060010768a4
Rzeigui Ahmed <ahmedrzeigui52@gmail.com>
2025-02-09 11:35:44 +0100

    Intégration primaire de la sérialisation binaire et xml dans l'application

--- GestionBibliothequeRzeiguiAhmed/Program.cs
+++ GestionBibliothequeRzeiguiAhmed/Program.cs
@@ -111,6 +111,31 @@ int chercherIndexUtilisateur(string nomUtilisateur,string prenomUtilisateur)
     }
     return i;
 }
+
+/***********************************************************************************************************
+ * 
+ * 
+ * 
+ * 
+ *               Serialisation  
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * **********************************************************************************************************/
+
+
+
+
+
+string chemin = "C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\GestionBibliothequeRzeiguiAhmed\\Bibliotheque";
+
+
+
+
+
 /***********************************************************************************************************
  * 
  * 
@@ -143,6 +168,9 @@ while (run)
         case "1":
             afficherLivresBibliothèques();
             break;
+
+
+
         case "2":
             Console.WriteLine("Insciption d'un Nouveau Utilisateur :) \n");
             Console.WriteLine("Entrer le nom de l'utilisateur : ");
@@ -157,7 +185,51 @@ while (run)
             string nomLivre = Console.ReadLine();
             Utilisateur newUtilisateur = new Utilisateur(nom, prenom, email, dateinscription, livres[chercherIndexLivre(nomLivre)]);
             Utilisateurs.Add(newUtilisateur);
+
+            Console.WriteLine("Sauvegarde Utilisateur : Choisisser type de sauvegarde de l'utilisateur : \n" +
+                              "0 --- Pour ---- Serialisation Binaire (.bjoson) \n" +
+                              "1 --- Pour ---- Serialisation XML (.xml) \n");
+            int typeserialistion = int.Parse(Console.ReadLine());
+            switch (typeserialistion)
+            {
+                case 0:
+
+                    SerializationFactory.sauvegarder(chemin, TypeSerialization.binaire, newUtilisateur, "1111");
+                    string cheminbjson = $"C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\GestionBibliothequeRzeiguiAhmed\\Bibliotheque\\Utilisateur_{newUtilisateur.Nom}_{newUtilisateur.Prenom}.bjson";
+                    Utilisateur utilisateurBjson = (Utilisateur)SerializationFactory.Charger(null, cheminbjson, typeof(Utilisateur), TypeSerialization.binaire);
+                    utilisateurBjson.afficherUtilisateur();
+                    break;
+                case 1:
+                    SerializationFactory.sauvegarder(chemin, TypeSerialization.XML, newUtilisateur, "1111");
+                    string chemin1 = $"C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\GestionBibliothequeRzeiguiAhmed\\Bibliotheque\\Utilisateur_{newUtilisateur.Nom}_{newUtilisateur.Prenom}.xml";
+                    string chemin2 = $"C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\GestionBibliothequeRzeiguiAhmed\\Bibliotheque\\Utilisateur_{newUtilisateur.Nom}_{newUtilisateur.Prenom}.hash";
+                    Utilisateur utilisateurXml = (Utilisateur)SerializationFactory.Charger(chemin2, chemin1, typeof(Utilisateur), TypeSerialization.XML);
+                    utilisateurXml.afficherUtilisateur();
+                    break;
+                default:
+                    Console.WriteLine("typeSerialisation incorrect");
+                    break;
+            }
+            //test Serialisation binaire : 
+            //SerializationFactory.sauvegarder(chemin, TypeSerialization.binaire, newUtilisateur, "1111");
+            //string chemin1 = $"C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\GestionBibliothequeRzeiguiAhmed\\Bibliotheque\\Utilisateur_{newUtilisateur.Nom}_{newUtilisateur.Prenom}.bjson";
+            //Utilisateur utilisateur = (Utilisateur)SerializationFactory.Charger(null, chemin1, typeof(Utilisateur), TypeSerialization.binaire);
+            //utilisateur.afficherUtilisateur();
+
+            //test Serialisation xml : 
+            //SerializationFactory.sauvegarder(chemin, TypeSerialization.XML, newUtilisateur, "1111");
+            //string chemin1 = $"C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\GestionBibliothequeRzeiguiAhmed\\Bibliotheque\\Utilisateur_{newUtilisateur.Nom}_{newUtilisateur.Prenom}.xml";
+            //string chemin2 = $"C:\\Users\\Rzeigui Ahmed\\Documents\\CS\\Gestion-d-une-biblioth-que-en-C-avec-s-rialisation-et-cryptage\\GestionBibliothequeRzeiguiAhmed\\Bibliotheque\\Utilisateur_{newUtilisateur.Nom}_{newUtilisateur.Prenom}.hash";
+            //Utilisateur utilisateur = (Utilisateur)SerializationFactory.Charger(chemin2, chemin1, typeof(Utilisateur), TypeSerialization.XML);
+            //utilisateur.afficherUtilisateur();
+
+
+
             break;
+
+
+
+
         case "3":
             Console.WriteLine("Donner nom de l'utilisateur");
             string nom_input = Console.ReadLine();
@@ -165,15 +237,22 @@ while (run)
             string prenom_input = Console.ReadLine();
             Utilisateurs[chercherIndexUtilisateur(nom_input, prenom_input)].afficherUtilisateur();
             break;
+
+
+
         case "4":
+            Console.WriteLine("Toute l'équipe vous exprime sa gratitude pour l'utilisation de notre bibliothèque");
             run = false;
             break;
+
+
+
         case "5":
             if (Utilisateurs.Count != 0)
             {
-                foreach (var utilisateur in Utilisateurs)
+                foreach (var user in Utilisateurs)
                 {
-                    utilisateur.afficherUtilisateur();
+                    user.afficherUtilisateur();
                 }
             }
             else
@@ -181,6 +260,9 @@ while (run)
                 Console.WriteLine("Aucun Utilisateur est connecté");
             }
             break;
+
+
+
         default:
             break;
     }
---
+++ GestionBibliothequeRzeiguiAhmed/SerializationFactory.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+using System;
+using System.IO;
+using System.Text.Json;
+using System.IO.Compression;
+using System.Xml.Serialization;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+public enum TypeSerialization
+{
+	binaire,
+	XML 
+}
+
+
+public class SerializationFactory
+{
+    public static void sauvegarder(string chemin, TypeSerialization typeserialization, object obj, string motDePasse)
+    {
+        if (typeserialization == TypeSerialization.binaire)
+        {
+            string json = JsonSerializer.Serialize(obj);
+            byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(json);
+
+
+            string hashMotDePasse = Convert.ToBase64String(System.Security.Cryptography.SHA256.HashData(Encoding.UTF8.GetBytes(motDePasse)));
+            byte[] hashBytes = Encoding.UTF8.GetBytes(hashMotDePasse);
+
+
+
+            chemin = Path.Combine(chemin, $"{obj.GetType().Name}_{obj.GetType().GetProperty("Nom").GetValue(obj)?.ToString()}_{obj.GetType().GetProperty("Prenom").GetValue(obj)?.ToString()}.bjson");
+            using (FileStream fileStream = new FileStream(chemin, FileMode.Create))
+            using (GZipStream compressionStream = new GZipStream(fileStream, CompressionMode.Compress))
+            {
+                compressionStream.Write(hashBytes, 0, hashBytes.Length);
+                compressionStream.Write(jsonBytes, 0, jsonBytes.Length);
+            }
+
+            Console.WriteLine($"Données sérialisées et compressées dans : {chemin}");
+        }
+        else
+        {
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
+            string chemin1 = Path.Combine(chemin, $"{obj.GetType().Name}_{obj.GetType().GetProperty("Nom").GetValue(obj)?.ToString()}_{obj.GetType().GetProperty("Prenom").GetValue(obj)?.ToString()}.xml");
+
+            using (FileStream fileStream = new FileStream(chemin1, FileMode.Create))
+            {
+                serializer.Serialize(fileStream, obj);
+            }
+
+
+            Console.WriteLine($"Données sérialisées en XML dans : {chemin1}");
+
+            Console.WriteLine("Entrer un mot de passe pour protéger ce fichier XML :");
+
+            // Générer un hash du mot de passe
+            string passwordHash = Convert.ToBase64String(System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(motDePasse)));
+
+            // Sauvegarder le hash dans un fichier associé
+            string hashFilePath = Path.Combine(chemin, $"{obj.GetType().Name}_{obj.GetType().GetProperty("Nom").GetValue(obj)?.ToString()}_{obj.GetType().GetProperty("Prenom").GetValue(obj)?.ToString()}.hash"); ;
+            File.WriteAllText(hashFilePath, passwordHash);
+
+            Console.WriteLine($"Mot de passe hashé et sauvegardé dans : {hashFilePath}");
+        }
+    }
+    public static object Charger(string chemin2, string chemin, Type typeObjet, TypeSerialization typeserialization)
+    {
+        if (typeserialization == TypeSerialization.binaire)
+        {
+
+            if (!File.Exists(chemin))
+            {
+                throw new FileNotFoundException($"Fichier introuvable : {chemin}");
+            }
+            int tentatives = 3;
+
+            while (tentatives > 0)
+            {
+                Console.WriteLine("Entrer Mot de passe");
+                string mdp = Console.ReadLine();
+                string hashMdp = Convert.ToBase64String(System.Security.Cryptography.SHA256.HashData(Encoding.UTF8.GetBytes(mdp)));
+
+
+                using (FileStream fileStream = new FileStream(chemin, FileMode.Open))
+                using (GZipStream decompressionStream = new GZipStream(fileStream, CompressionMode.Decompress))
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    // Lire le hash du fichier
+                    byte[] hashBytes = new byte[44]; // Taille d'un hash SHA256 encodé en Base64
+                    decompressionStream.Read(hashBytes, 0, hashBytes.Length);
+                    string hashFichier = Encoding.UTF8.GetString(hashBytes);
+
+                    if (hashMdp == hashFichier)
+                    {
+                        decompressionStream.CopyTo(memoryStream);
+                        byte[] decompressedBytes = memoryStream.ToArray();
+                        string json = System.Text.Encoding.UTF8.GetString(decompressedBytes);
+
+
+                        object obj = JsonSerializer.Deserialize(json, typeObjet);
+                        Console.WriteLine($"Données désérialisées depuis : {chemin}");
+                        return obj;
+                    }
+                    else
+                    {
+                        tentatives--;
+                        Console.WriteLine($"Mot de passe incorrect. Il vous reste {tentatives} tentatives.");
+                    }
+                }
+            }
+            if (tentatives == 0)
+            {
+                Console.WriteLine("3 tentatives échouées. Le fichier sera supprimé pour garantir la sécurité.");
+                File.Delete(chemin);
+                throw new UnauthorizedAccessException("Accès refusé. Fichier supprimé.");
+            }
+            throw new InvalidOperationException("Impossible de charger le fichier. Vérifiez le mot de passe.");
+        }
+        else
+        {
+            if (!File.Exists(chemin))
+            {
+                throw new FileNotFoundException($"Fichier introuvable : {chemin}");
+            }
+            // Vérifiez si un fichier de hash existe
+            string cheminHash = chemin2;
+            if (!File.Exists(cheminHash))
+            {
+                throw new FileNotFoundException($"Fichier de hash introuvable : {cheminHash}");
+            }
+
+            // Lire le hash depuis le fichier
+            string hashFichier;
+            using (StreamReader reader = new StreamReader(cheminHash))
+            {
+                hashFichier = reader.ReadLine();
+            }
+
+            if (string.IsNullOrEmpty(hashFichier))
+            {
+                throw new InvalidOperationException("Fichier de hash corrompu ou vide.");
+            }
+
+            // Demander le mot de passe et vérifier le hash
+            int tentatives = 3;
+            while (tentatives > 0)
+            {
+                Console.WriteLine("Entrer le mot de passe :");
+                string mdp = Console.ReadLine();
+
+                // Générer le hash du mot de passe entré
+                string hashMdp = Convert.ToBase64String(System.Security.Cryptography.SHA256.HashData(Encoding.UTF8.GetBytes(mdp)));
+
+                // Comparer le hash calculé avec celui du fichier
+                if (hashMdp == hashFichier)
+                {
+                    // Si le mot de passe est correct, désérialiser le fichier XML
+                    XmlSerializer serializer = new XmlSerializer(typeObjet);
+                    using (FileStream fileStream = new FileStream(chemin, FileMode.Open))
+                    {
+                        object obj = serializer.Deserialize(fileStream);
+                        Console.WriteLine($"Données désérialisées depuis : {chemin}");
+                        return obj;
+                    }
+                }
+                else
+                {
+                    tentatives--;
+                    Console.WriteLine($"Mot de passe incorrect. Il vous reste {tentatives} tentative(s).");
+                }
+            }
+
+            // Si les tentatives sont épuisées, supprimer le fichier pour des raisons de sécurité
+            if (tentatives == 0)
+            {
+                Console.WriteLine("3 tentatives échouées. Le fichier sera supprimé pour garantir la sécurité.");
+                File.Delete(chemin);
+                File.Delete(cheminHash); // Supprime également le fichier de hash
+                throw new UnauthorizedAccessException("Accès refusé. Fichier supprimé.");
+            }
+
+            throw new InvalidOperationException("Impossible de charger le fichier. Vérifiez le mot de passe.");
+        }
+    }
+}
